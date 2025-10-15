8c5d5ba5a3d721ee853952d4862975e55649b8db
solvam <alumno@solvam.local>
2025-10-15 13:52:07 +0200

    todo categorias

--- tema3/Libreria_alumos/admin/index.php
+++ tema3/Libreria_alumos/admin/index.php
@@ -1,8 +1,21 @@
+<?php
+require_once("./includes/sessions.php");
+$sesion = new Sessions();
+
+if (!$sesion->comprobarSesion()) {
+    header("Location: ../login.php");
+    exit();
+}
+
+$usuario = $_SESSION['usuario'];//array con los datos
+
+?>
+
 <!DOCTYPE html>
 <html lang="es">
 <head>
     <meta charset="UTF-8">
-    <title>Panel de administración</title>
+    <title>Panel de administracion</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
@@ -13,8 +26,8 @@
 <div class="container mt-4">
     <div class="row justify-content-center">
         <div class="col-md-10">
-            <h2 class="mb-4">Bienvenido al panel de administración</h2>
-            <p>Hola, <strong>Nombre y apellidos</strong>. Has accedido correctamente al área privada.</p>
+            <h2 class="mb-4">Bienvenido al panel de administracion</h2>
+            <p>Hola, <strong> <?= $usuario['nombre']?> <?= $usuario['apellidos']?></strong>. Has accedido correctamente al area privada.</p>
         </div>
     </div>
 </div>
